eb00e7c315eef31942ed5ebd585b5daeac47596e
quy145 <quynb2003@gmail.com>
2023-09-11 22:39:13 +0700

    Upload: ProductDetail+Checkout

--- React_DATN/src/App.jsx
+++ React_DATN/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom"
-import viteLogo from '/vite.svg'
-import './App.css'
 import Layout_Web from './Page/Layout/Layout_Web'
 import Contact from "./Page/Contact"
 import Bill from "./Page/Bill"
 import Cart from "./Page/Cart"
+import ProductDetail from "./Page/ProductDetail"
+import Checkout from "./Page/Checkout"
+
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
         <Route path="cart" element={<Cart />} />
         <Route path="bill" element={<Bill />} />
         <Route path="contact" element={<Contact />} />
+        <Route path="product-detail" element={<ProductDetail />} />
+        <Route path="checkout" element={<Checkout />} />
+
       </Route>
     </Routes>
   )
